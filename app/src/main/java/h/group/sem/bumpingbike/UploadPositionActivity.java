60ad70c8df90bfe551dc58504b7ffb905fd9028e
msjaelland <mortenrs12@hotmail.com>
2017-10-30 21:56:55 +0100

    Working on real location functionality

--- app/src/main/java/h/group/sem/bumpingbike/UploadPositionActivity.java
+++ app/src/main/java/h/group/sem/bumpingbike/UploadPositionActivity.java
@@ -2,25 +2,36 @@ package h.group.sem.bumpingbike;
 
 import android.app.Activity;
 import android.content.Context;
+import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApi;
+import com.google.android.gms.location.LocationServices;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 /**
  * Created by Morten on 30-10-2017.
  */
 
-public class UploadPositionActivity extends AppCompatActivity {
+public class UploadPositionActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener{
 
     EditText longitude;
     EditText latitude;
+    Location mLocation;
     DatabaseReference databasePositions;
+    GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +40,12 @@ public class UploadPositionActivity extends AppCompatActivity {
         setVariables();
         //Get database reference
         databasePositions = FirebaseDatabase.getInstance().getReference("position");
+        //Get google api client
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
     }
 
     public void setVariables(){
@@ -37,14 +54,42 @@ public class UploadPositionActivity extends AppCompatActivity {
     }
 
     public void handleUpBtn(View view){
-        double longitude = Double.parseDouble(this.longitude.getText().toString().trim());
-        double latitude = Double.parseDouble(this.latitude.getText().toString().trim());
+            if (mLocation != null){
+                String id = databasePositions.push().getKey();
+                Position pos = new Position(id, mLocation.getLongitude(), mLocation.getLatitude());
 
-        String id = databasePositions.push().getKey();
-        Position pos = new Position(id, longitude, latitude);
-
-        databasePositions.child(id).setValue(pos);
-        Toast.makeText(this, "Position added", Toast.LENGTH_LONG).show();
+                databasePositions.child(id).setValue(pos);
+                Toast.makeText(this, "Position added", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, "No last location", Toast.LENGTH_LONG).show();
+            }
     }
 
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        Toast.makeText(this, "Google API connected...", Toast.LENGTH_LONG).show();
+        mLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Toast toast = Toast.makeText(this, "Failed connection", Toast.LENGTH_LONG);
+        toast.show();
+    }
+
+    protected void onStart() {
+        mGoogleApiClient.connect();
+        super.onStart();
+    }
+
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
 }
