42f8053696086989114dbbcc2b2f6eeb117a66d8
María Karlsdóttir <karlsdottir.maria@gmail.com>
2017-11-13 23:49:19 +0100

    cleanup

--- app/src/main/java/h/group/sem/bumpingbike/TopBumpsActivity.java
+++ app/src/main/java/h/group/sem/bumpingbike/TopBumpsActivity.java
@@ -1,19 +1,16 @@
 package h.group.sem.bumpingbike;
 
-import android.app.ActionBar;
+import android.app.Activity;
 import android.location.Location;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.text.Layout;
-import android.view.Gravity;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.ProgressBar;
-import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
@@ -27,16 +24,6 @@ import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.Map;
-
-import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.TextView;
 
 import static java.lang.Math.toRadians;
 
